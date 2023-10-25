46bf4f5718c9560fc2a3490de6839be1f5a94988
changho <chseo@hyundaiht.co.kr>
2023-10-25 16:00:54 +0900

    build setting

---
+++ Projects/Sample/Sources/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by changho seo on 10/25/23.
+//  Copyright © 2023 com.hyundaiht. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SeoAppKit
+
+
+class ViewController:UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        SeoAppKit.hello()
+        
+    }
+    
+}
